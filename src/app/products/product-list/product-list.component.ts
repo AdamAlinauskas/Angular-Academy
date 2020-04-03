e9d61c9a50d1daa92d8821dad073f7bed66d5460
Adam Alinauskas <adam.alinauskas@energysafetycanada.com>
2020-04-03 09:52:24 -0600

    Error catching observable, paging data.

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
-import { Observable } from "rxjs";
+import { Observable, EMPTY } from "rxjs";
 import { ProductService } from "./../product.service";
 import { Product } from "./../product.interface";
 import { Component, OnInit } from "@angular/core";
+import { catchError } from "rxjs/operators";
+// import { catchError, delay, shareReplay, tap, map } from "rxjs/operators";
 
 @Component({
   selector: "app-product-list",
@@ -13,6 +15,25 @@ export class ProductListComponent implements OnInit {
   products: Product[];
   selectedProduct: Product;
   products$: Observable<Product[]>;
+  errorMessage: string;
+
+  //Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  nextPage(): void {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+  }
+
+  previousPage(): void {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+  }
 
   onSelect(product: Product) {
     console.log(product);
@@ -22,7 +43,12 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.products$ = this.productService.products$;
+    this.products$ = this.productService.products$.pipe(
+      catchError(error => {
+        this.errorMessage = error;
+        return EMPTY;
+      })
+    );
 
     // this.productService.products$.subscribe(
     //   results => (this.products = results)
